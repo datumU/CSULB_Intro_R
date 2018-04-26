d5fabb02227d9cd10cb343b3b175d32a2d35df35
Micah <gmicahjackson@gmail.com>
2018-04-26 13:58:23 -0700

    updated slides, complete and shell code

--- day_2/slides/session_1/complete_code.R
+++ day_2/slides/session_1/complete_code.R
@@ -1,10 +1,23 @@
 ####################################################################################
-### Exploratory Data Analysis - Complete code from slides
-### Author: Chris Galbraith 
+### Exploratory Data Analysis - Minimal and complete code from slides 
+### Author: Chris Galbraith, Micah Jackson
 ####################################################################################
+
+install.packages('vioplot', dependencies=TRUE)
+library(vioplot)
+
+## Data Frame Review
+myDF <- data.frame(names=c('Albert', 'Bianca', 'Cat'), height=c(71, 57, 64), female=c(0,1,1))
+
+
 ## str()
-str(str)
+str(mean)
+str(matrix)
 str(sample)
+
+
+## str()
+str(myDF)
 
 prestige <- read.table(file = here::here("data", "prestige_v2.csv"), 
                        sep=",", 
@@ -13,61 +26,84 @@ prestige <- read.table(file = here::here("data", "prestige_v2.csv"),
 str(prestige)
 
 
-## EDA - Numeric Summary
+## 5 number summary
 summary(prestige)
 
 
+## boxplots
+boxplot(prestige$prestige)
+boxplot(prestige$prestige, horizontal=TRUE)
+boxplot(prestige$prestige, horizontal=TRUE, xlab='Prestige Scores')
+
+
 ## Histograms
+hist(prestige$prestige)
+hist(prestige$prestige, freq = FALSE)
+hist(prestige$prestige, freq = FALSE, 
+  col = "grey")
 hist(prestige$prestige, freq = FALSE, 
      col = "grey",
      main = "Histogram of Prestige Score",
      xlab = "Prestige Score")
-abline(v = median(prestige$prestige), col = "blue", lty = 2, lwd = 2)
 
 
 ## Histogram with overlaid density 
-hist(prestige$prestige, freq=FALSE, col = "grey", main = "", xlab = "Prestige Score", ylim=c(0, 0.022))
-abline(v = median(prestige$prestige), col = "blue", lty = 2, lwd = 2)
-lines(density(prestige$prestige), col = "red", lwd = 2)
+hist(prestige$prestige, freq=FALSE, col = "grey", main = "", xlab = "Prestige Score", ylim=c(0,0.022))
+lines(density(prestige$prestige, bw='nrd0'), col = "red", lwd = 2)
+lines(density(prestige$prestige, bw='SJ'), col = "green", lwd = 2)
+lines(density(prestige$prestige, bw=0.5), col = "blue", lwd = 2)
+lines(density(prestige$prestige, bw=5), col = "purple", lwd = 2)
 
 
-## Adding a Legend
-hist(prestige$prestige, freq=FALSE, col = "grey", main = "", xlab = "Prestige Score", ylim=c(0, 0.022))
-abline(v = median(prestige$prestige), col = "blue", lty = 2, lwd = 2)
-lines(density(prestige$prestige), col = "red", lwd = 2)
-legend("topright", legend = c("Median", "Density Est."),
-       col = c("blue", "red"), lty = c(2, 1), lwd = 2, bty = "n")
+## Violin Plots
+vioplot(prestige$prestige)
+vioplot(prestige$prestige, horizontal=TRUE, col='dodgerblue2',
+  names='Prestige Scores')
 
 
-## Boxplots
-summary(prestige$prestige)
-boxplot(prestige$prestige, horizontal = TRUE, xlab = "Prestige Score")
+## Overlay histograms
+pp <- prestige$prestige
+hist(pp[prestige$type=='bc'], breaks=10, main='Histograms of Prestige Scores by Job Type',
+  xlab='Prestige Scores', col='dodgerblue2')
+hist(pp[prestige$type=='wc'], breaks=10, col='green')
+hist(pp[prestige$type=='prof'], breaks=10, col='purple')
+
+hist(pp[prestige$type=='bc'], breaks=10, main='Histograms of Prestige Scores by Job Type',
+  xlab='Prestige Scores', col=adjustcolor('dodgerblue2',.4), xlim=c(0,100))
+hist(pp[prestige$type=='wc'], breaks=10, col=adjustcolor('green', .4), add=TRUE)
+hist(pp[prestige$type=='prof'], breaks=10, col=adjustcolor('purple', .4), add=TRUE)
 
 
-## Grouped Boxplots
-boxplot(prestige ~ type, data = prestige, col = "grey",
-        main = "Distribution of Prestige Score by Type of Occupation",
-        xlab = "Occupation Type", ylab = "Prestige Score")
+## Adding lines
+abline(v=mean(pp[prestige$type=='bc']), lwd=2, col='dodgerblue2')
+abline(v=mean(pp[prestige$type=='wc']), lwd=2, col='green')
+abline(v=mean(pp[prestige$type=='prof']), lwd=2, col='purple')
 
+
+## Adding legends
+legend("topright", legend = c("BC", "WC", "PROF"),
+  col = c("dodgerblue2", "green", "purple"), lty = 1, lwd = 2, bty = "n")
+
+
+## Grouped Violin plots
+vioplot(pp[prestige$type=='bc'], pp[prestige$type=='wc'], pp[prestige$type=='prof'],
+  horizontal=TRUE, names=c('BC', 'WC', 'PROF'))
 
 ## Scatterplots
-plot(x = prestige$education, y = prestige$prestige, type = "p", pch = 20,
-     main = "Prestige Score by Education",
-     xlab = "Avg Years of Education", ylab = "Prestige Score")
+plot(x = prestige$education, y = prestige$prestige, pch = 20,
+  main = "Prestige Score by Education",
+  xlab = "Avg. Years of Education", ylab = "Prestige Score")
 
 
 ## Scatterplots, contd.
-plot(prestige$education, prestige$prestige, type = "p", pch = 20,
-     main = "Prestige Score by Education",
-     xlab = "Ave. Years of Education", 
-     ylab = "Prestige Score")
-abline(reg = lm(prestige ~ education, data = prestige), col = "green", lwd = 2)  # linear regression
-lines(lowess(x = prestige$education, y = prestige$prestige), col = "red", lwd = 2)  # smoother
-legend("topleft", bty = "n",
-       legend = c("Regression Line", "Smoother"), 
-       col = c("green", "red"),
-       lwd = c(2,2), 
-       lty = 1)
+abline(reg = lm(prestige ~ education, data = prestige),
+  col = "green", lwd = 2)  # linear regression
+
+lines(lowess(x = prestige$education, y = prestige$prestige),
+  col = "red", lwd = 2)  # smoother
+
+legend("topleft", legend = c("Regression Line", "Smoother"),
+  col = c("green", "red"), lwd = c(2,2), lty = 1, bty = "n")
 
 
 ## Scatterplot Matrices
--- day_2/slides/session_1/shell_code.R
+++ day_2/slides/session_1/shell_code.R
@@ -1,78 +1,102 @@
 ####################################################################################
-### Exploratory Data Analysis - Shell code from slides
+### Exploratory Data Analysis - Minimal/shell code from slides 
+### Author: Chris Galbraith, Micah Jackson
 ####################################################################################
+
+install.packages('vioplot', dependencies=TRUE)
+library(vioplot)
+
+## Data Frame Review
+myDF <- data.frame(names=c('Albert', 'Bianca', 'Cat'), height=c(71, 57, 64), female=c(0,1,1))
+
+
 ## str()
-str(str)
+str(mean)
+str(matrix)
 str(sample)
-
-prestige <- read.table(file = here::here("data", "prestige_v2.csv"),
-                       sep=",", 
-                       header=TRUE, 
-                       row.names=1)
+?str
 
 
+## str()
+str(myDF)
 
-## EDA - Numeric Summary
+prestige <- read.table(file = here::here("data", "prestige_v2.csv"), 
+  sep=",", 
+  header=TRUE, 
+  row.names=1)
+str(prestige)
 
+
+## 5 number summary
+summary(prestige)
+?summary
+
+
+## boxplots
+boxplot(prestige$prestige)
+?boxplot
 
 
 ## Histograms
+hist(prestige$prestige)
 ?hist
-hist(prestige$prestige, freq = FALSE, 
-     col = "grey",
-     main = "Histogram of Prestige Score",
-     xlab = "Prestige Score")
-?abline
-abline(v = median(prestige$prestige), col = "blue", lty = 2, lwd = 2)
 
 
 ## Histogram with overlaid density 
+hist(prestige$prestige, freq=FALSE, col = "grey", main = "", xlab = "Prestige Score", ylim=c(0,0.022))
+lines(density(prestige$prestige, bw='nrd0'), col = "red", lwd = 2)
 ?lines
 ?density
-hist(prestige$prestige, freq=FALSE, col = "grey", main = "", xlab = "Prestige Score", ylim=c(0, 0.022))
-abline(v = median(prestige$prestige), col = "blue", lty = 2, lwd = 2)
 
 
-## Adding a Legend
+## Violin Plots
+vioplot(prestige$prestige)
+?vioplot
+
+
+## Overlay histograms
+pp <- prestige$prestige
+hist(pp[prestige$type=='bc'], breaks=10, main='Histograms of Prestige Scores by Job Type',
+  xlab='Prestige Scores', col='dodgerblue2')
+hist(pp[prestige$type=='wc'], breaks=10, col='green')
+hist(pp[prestige$type=='prof'], breaks=10, col='purple')
+
+
+## Adding lines
+?abline
+abline(v=mean(pp[prestige$type=='bc']), lwd=2, col='dodgerblue2')
+abline(v=mean(pp[prestige$type=='wc']), lwd=2, col='green')
+abline(v=mean(pp[prestige$type=='prof']), lwd=2, col='purple')
+
+
+## Adding legends
 ?legend
-hist(prestige$prestige, freq=FALSE, col = "grey", main = "", xlab = "Prestige Score", ylim=c(0, 0.022))
-abline(v = median(prestige$prestige), col = "blue", lty = 2, lwd = 2)
-lines(density(prestige$prestige), col = "red", lwd = 2)
+legend("topright", legend = c("BC", "WC", "PROF"),
+  col = c("dodgerblue2", "green", "purple"), lty = 1, lwd = 2, bty = "n")
 
 
-## Boxplots
-
-
-
-## Grouped Boxplots
-boxplot(
-        col = "grey",
-        main = "Distribution of Prestige Score by Type of Occupation",
-        xlab = "Occupation Type", 
-        ylab = "Prestige Score")
+## Grouped Violin plots
+vioplot(pp[prestige$type=='bc'], pp[prestige$type=='wc'], pp[prestige$type=='prof'],
+  horizontal=TRUE, names=c('BC', 'WC', 'PROF'))
 
 
 ## Scatterplots
-plot(
-     main = "Prestige Score by Education",
-     xlab = "Avg Years of Education", ylab = "Prestige Score")
+?plot
+plot(x = prestige$education, y = prestige$prestige, pch = 20)
 
 
 ## Scatterplots, contd.
-plot(prestige$education, prestige$prestige, type = "p", pch = 20,
-     main = "Prestige Score by Education",
-     xlab = "Ave. Years of Education", 
-     ylab = "Prestige Score")
-?abline
-abline(, col = "green", lwd = 2)  # linear regression
-?lowess
-lines(, col = "red", lwd = 2)  # smoother
-legend("topleft", bty = "n",
-       legend = c("Regression Line", "Smoother"), 
-       col = c("green", "red"),
-       lwd = c(2,2), 
-       lty = 1)
+abline(reg = lm(prestige ~ education, data = prestige),
+  col = "green", lwd = 2)  # linear regression
+
+lines(lowess(x = prestige$education, y = prestige$prestige),
+  col = "red", lwd = 2)  # smoother
+
+legend("topleft", legend = c("Regression Line", "Smoother"),
+  col = c("green", "red"), lwd = c(2,2), lty = 1, bty = "n")
 
 
 ## Scatterplot Matrices
 library(car)
+scatterplotMatrix( prestige[ ,c("prestige","education","income","women")] )
+?scatterplotMatrix
