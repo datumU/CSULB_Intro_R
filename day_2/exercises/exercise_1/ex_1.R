00935a360812bee2bdd09b6ad035ebd56668e838
Micah <gmicahjackson@gmail.com>
2018-04-26 20:16:29 -0700

    tweak session 1 slides. updated exercise and solutions 1.

--- day_2/exercises/exercise_1/ex_1.R
+++ day_2/exercises/exercise_1/ex_1.R
@@ -1,89 +1,71 @@
 ####################################################################################
 ### EXERCISE 1 Solutions
-### Author: Chris Galbraith 
+### Author: Chris Galbraith, Micah Jackson 
 ####################################################################################
-### PART A - Data Input ###
-# A.2
-auto <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data-original")
 
-# A.3
+### Part A: Data Input
+auto <- read.table("https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data-original")
 names(auto) <- c("mpg", "cyl", "disp", "hp", "weight", "acc", "model.yr", "origin", "name")
 
-# A.4
 auto$cyl <- factor(auto$cyl)
 auto$name <- as(auto$name, "character")
 
-# A.5
 head(auto)
+head(auto, n=10)
 
 
-### PART B - Missing Values ###
-# B.1
-missing <- which(is.na(auto), arr.ind = TRUE)
-missing
+### Part B - String Manipulation
 
-# B.2
-auto[missing[,1], ]
-
-
-### PART C - Sorting ###
-# C.1
-sort.index <- order(auto$mpg, decreasing = TRUE, na.last = NA)
-auto.sorted <- auto[sort.index, ]
-
-# C.2
-head(auto.sorted, 5)
-
-# C.3
-tail(auto.sorted, 5)
-
-
-### PART D - String Manipulation ###
-# D.1
 diesel.index <- grep("diesel", auto$name)
 diesel.index
 
-# D.2 
-auto$diesel <- 0
-auto$diesel[diesel.index] <- 1
+auto$diesel <- 0 # creates new column of all 0s
+auto$diesel[diesel.index] <- 1 # assigns 1s to all autos with diesel in the name
 
-# D.3
-auto$diesel <- factor(auto$diesel)
+auto$diesel <- as.factor(auto$diesel)
 
-# D.4
 str(auto)
 
-# D.5
 save(auto, file=here::here("data", "auto_mpg_v2.Rda"))
+?save
 
 
-####################################################################################
-### EXERCISE 2 Solutions
-### Author: Chris Galbraith
-####################################################################################
-### PART A ###
-# A.1
-load(here::here("data", "auto_mpg_v2.Rda"))
+### Part C
 
-# A.2
 summary(auto)
 
-### PART B ###
-# B.1
-hist(auto$mpg, col = "grey", freq=FALSE, main = "Histogram of MPG", xlab = "MPG")
 
-# B.2
-lines(density(auto$mpg, na.rm=TRUE), col="red")
+### Part D
 
-# B.3
+hist(auto$mpg, freq=FALSE, col = "grey", main = "Histogram of MPG", xlab = "MPG")
+
+hist(auto$mpg, freq=FALSE, col = "grey", main = "Histogram of MPG", xlab = "MPG")
+lines(density(auto$mpg, na.rm=TRUE), col=adjustcolor("red", .5))
+
+hist(auto$mpg, freq=FALSE, col = "grey", main = "Histogram of MPG", xlab = "MPG")
+lines(density(auto$mpg, na.rm=TRUE), col=adjustcolor("red", .5), main="Densities of Samp1 and Samp2", xlab="")
 abline(v = median(auto$mpg, na.rm=TRUE), col = "red", lwd = 2, lty=2)
 
-### PART C ###
-boxplot(mpg ~ cyl, data = auto, col = "grey",
-        main = "Distribution of MPG by Number of Cylinders",
-        xlab = "Cylinders", ylab = "MPG")
 
-### PART D ###
+### Part E
+
+# Several options for getting this answer. 
+summary(auto)
+# or 
+summary(auto$cyl)
+# or
+unique(auto$cyl)
+
+cylCounts <- table(auto$cyl)
+
+boxplot(mpg ~ cyl, data = auto, col = ifelse(cylCounts < 5, 'red', 'blue'),
+main = "Distribution of MPG by Number of Cylinders",
+xlab = "Cylinders", ylab = "MPG")
+
+
+
+### Part F
+
 library(car)
-scatterplotMatrix(auto[,c("mpg", "cyl", "disp", "hp", "weight", "acc", "model.yr")])
+scatterplotMatrix( auto[, c("mpg", "cyl", "disp", "hp", "weight", "acc", "model.yr")] )
 
